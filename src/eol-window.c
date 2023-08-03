5581b3da3e91707190b5cbb161769f4436d12df7
TogekissTube <138976298+TogekissTube@users.noreply.github.com>
2023-08-03 08:42:37 +0200

    Main window
    Main window

---
+++ src/eol-window.c
@@ -0,0 +1,10 @@
+/*
+    eol-window.c
+    Description: This file contains the implementation of the main window of the image viewer.
+   
+    In this window, images are uploaded and displayed, and user interactions are managed.
+    Navigation controls, zoom and other essential functionalities are also defined.
+    Take advantage of GTK+ 4 features to provide a fluid and intuitive user experience.
+
+    Contributions and improvements are welcome. Feel free to explore the code and participate in the development.
+*/
